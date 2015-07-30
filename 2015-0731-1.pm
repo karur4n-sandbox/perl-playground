fe992c554d931a9a59f9a86e56ecb48eb632ef3d
karur4n <karuran.f@gmail.com>
2015-07-31 08:06:13 +0900

    mini fix

--- 2015-0731-1.pm
+++ 2015-0731-1.pm
@@ -2,17 +2,16 @@ use strict;
 use warnings;
 
 my $maximum = &max(3, 5, 10, 4, 6);
-print $maximum;
+print "$maximum\n";
 
 sub max {
   # 初めてのPerl 5版だと リストコンテキストにしてあるけどする必要ある？
   # my ($max_so_far) = shift @_;
   my $max_so_far = shift @_;
-  print ($max_so_far);
   foreach (@_) {
     if ($_ > $max_so_far) {
       $max_so_far = $_;
     }
   }
-  return $max_so_far;
+  $max_so_far;
 }
